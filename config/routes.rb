78d1004a70b16a86a9a5544fa2760fce9d0a8577
Anuj <anashkan123@gmail.com>
2015-06-15 06:29:12 +0000

    Second Commit - Comments in Introduction.txt and routes.rb and some code in application_controller.rb

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,10 @@ Rails.application.routes.draw do
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-  # root 'welcome#index'
+  root 'application#hello'
+  
+  #application is the controller and hello is the action
+  
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
